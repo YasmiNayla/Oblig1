aef7459f48dcd48d001f5022b6ee9e1572950789
Yassincase <yagar3121@oslomet.no>
2024-02-02 09:54:00 +0100

    wrote in javascript based on a module-lecture...

---
+++ src/main/resources/static/JavaScript.index.js
@@ -0,0 +1,48 @@
+function showName() {
+    document.getElementByld = function (name) {
+
+    };
+    let name = document.getElementByld("name").value;
+    if (name == text){
+        return true;
+    } else {
+        alert("You have to write the name you have on your Identification Card";
+    }
+}
+
+function showSurname() {
+    document.getElementByld = function (surname) {
+
+    };
+    let surname = document.getElementByld("surname").value;
+    if (surname == text){
+        return true;
+    } else {
+        alert("You have to write the surname you have on your Identification Card";
+    }
+}
+
+function showPhoneNr() {
+    document.getElementByld = function (PhoneNr) {
+
+    };
+    let PhoneNr = document.getElementByld("PhoneNr").value;
+    if (PhoneNr == numbers){
+        return true;
+    } else {
+        alert("The phone number is invalid, to get your buying confirmation write inn a valid phone number";
+    }
+}
+
+function Email() {
+    document.getElementByld = function (Email) {
+
+    };
+    let Email = document.getElementByld("Email").value;
+    if (Email == text){
+        return true;
+    } else {
+        alert("The email is invalid, to get your tickets write inn a valid email";
+    }
+}
+
