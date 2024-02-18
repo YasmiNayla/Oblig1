979ed730bec1af4ea732e0df15bcedcf7e0d2434
Yassincase <yagar3121@oslomet.no>
2024-02-18 15:06:56 +0100

    added validation codes. Not working though. Failing to connect

--- src/main/resources/static/JavaScript.index.js
+++ src/main/resources/static/JavaScript.index.js
@@ -1,26 +1,64 @@
+const userRegister[];  // Creation of an empty array
+
+    function showUserRegister() {
+        let out = "<table><tr>"
+        "<th>Film</th><th>Amount</th><th>Name</th><th>Surname</th><th>PhoneNumber</th><th>Email</th>"
+        "</tr>";
+        out + - = "</tr>";
+        {
+            $("#userRegister").html(out);
+        }
+    }
+
+      // Table from info inserted in input boxes
+
+    function register () {
+        const Movie = $("#Movie").val();
+        const Amount = $("#Amount").val();
+        const Name = $("#Name").val();
+        const Surname = $("#Surname").val();
+        const PhoneNr = $("#PhoneNr").val();
+        const Email = $("#Email").val();
+
+    const person = {
+        Movie : Movie,
+        Amount : Amount,
+        Name : Name,
+        PhoneNr : PhoneNr,
+        Email : Email
+    };
+userRegister.push(person);
+$('input').val('');
+
+showUserRegister()}    /// Shows the table
+
+////
+
+function reg () {
 
 function choose(movies){
     alert ("You will watch: "+ document.getElementById("movies").value);
 }
 
+for (let p of persRegister)
 function showAmount() {
     document.getElementById = function (Amount) {
 
+        let Amount = document.getElementById("Amount").value;
+        if (Amount >= 1){
+            return true;
+        }
+        if (Amount === '') {
+            $("#Movie").after(errorMessage);
+        }
     };
-    let Amount = document.getElementById("Amount").value;
-    if (Amount >= 1){
-        return true;
-    } else {
-        alert("Invalid Order: write a minimum of 1 tickets);
-    }
 }
-
 function showName() {
-    document.getElementById = function (name) {
+    document.getElementById = function (Name) {
 
     };
-    let name = document.getElementById("name").value;
-    if (name >= 3){
+    let Name = document.getElementById("Name").value;
+    if (Name >= 3){
         return true;
     } else {
         alert("You have to write the name you have on your Identification Card");
@@ -28,11 +66,11 @@ function showName() {
 }
 
 function showSurname() {
-    document.getElementById = function (surname) {
+    document.getElementById = function (Surname) {
 
     };
-    let surname = document.getElementById("surname").value;
-    if (surname >= 3){
+    let Surname = document.getElementById("Surname").value;
+    if (Surname >= 3){
         return true;
     } else {
         alert("You have to write the surname you have on your Identification Card");
@@ -43,8 +81,8 @@ function showPhoneNr() {
     document.getElementById = function (PhoneNr) {
 
     };
-    let PhoneNumber = document.getElementById("PhoneNumber").value;
-    if (PhoneNumber >= 8){
+    let PhoneNr = document.getElementById("PhoneNr").value;
+    if (PhoneNr >= 8){
         return true;
     } else {
         alert("The phone number is invalid, to get your buying confirmation write inn a valid phone number");
@@ -53,7 +91,19 @@ function showPhoneNr() {
 
 function Email() {
     document.getElementById = function (Email) {
-
+        const Email = document.getElementById("Email");
+            Email.addEventListener("blur", () => {
+                let regex =
+                /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+                let s = Email.value;
+                if (regex.test(s)) {
+                    Email.classList.remove("is-invalid");
+                    emailError = true;
+                } else {
+                    Email.classList.add("is-invalid");
+                    emailError = false;
+                }
+            });
     };
     let Email = document.getElementById("Email").value;
     if (Email >= 2 + "@" + 2 + "." + 2){
@@ -63,3 +113,56 @@ function Email() {
     }
 }
 
+}
+
+
+/// Red letters under the input fields if the box is empty
+function register () {
+    const Movie = $("#Movie").val();
+    const Amount = $("#Amount").val();
+    const Name = $("#Name").val();
+    const Surname = $("#Surname").val();
+    const PhoneNr = $("#PhoneNr").val();
+    const Email = $("#Email").val();
+
+    if (Movie === '' || Amount === '' || Surname === '' || PhoneNr === '' || Email) {
+        const errorMessage = "<span style ='color: Maroon; font-size: 12px'> Fill in all boxes</span";
+        if (Movie === '') {
+            $("#Movie").after(errorMessage);
+        }
+        if (Amount === '') {
+            $("#Amount").after(errorMessage);
+        }
+        if (Name === '') {
+            $("#Name").after(errorMessage);
+        }
+        if (Surame === '') {
+            $("#Surame").after(errorMessage);
+        }
+        if (PhoneNr === '') {
+            $("#PhoneNr").after(errorMessage);
+        }
+        if (Email === '') {
+            $("#Email").after(errorMessage);
+        }
+    } else {
+        const person = {
+            Movie: Movie,
+            Amount: Amount,
+            Name: Name,
+            PhoneNr: PhoneNr,
+            Email: Email
+        };
+        userRegister.push(person);
+        $('input').val('');
+
+        showUserRegister()
+    }            // Inserts information into array
+}
+
+
+// Clear all tickets stored in the momentary array.
+function clearAll() {
+        userRegister.length = 0;
+        showUserRegister();
+}
